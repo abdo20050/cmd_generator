899b3665c91f7e4d31170e93763ad002c1555851
abdo25 <aabdo20050@gmail.com>
2022-09-25 00:51:37 +0300

    use args for effeciant use

--- cmd_gen.py
+++ cmd_gen.py
@@ -2,19 +2,50 @@
 #This code is made only for educational purposes, any misuse programmer won't be responsible
 #Description: This code used to generate cmd files for "irsim" simulator
 import os
-def grayGen(num):
-    return num ^ (num >> 1)
 
+import sys, getopt
+
+fileName = ""
+stepSize = 50
+labels = [str("")]*20
+I = ""
+O = ""
+
+def main(argv):
+    global fileName
+    global I
+    global O
+    try:
+        opts, args = getopt.getopt(argv,"hn:i:o:",["filename=","iLabels=","oLabels="])
+    except getopt.GetoptError:
+        print ("cmd_gen.py -n <fileName> -i <inputLabels> -o <outputLabels>")
+        sys.exit()
+    for opt, arg in opts:
+            if opt == '-h':
+                print ("cmd_gen.py -n <fileName> -i <inputLabels> -o <outputLabels>")
+                sys.exit()
+            elif opt in ("-n", "--fileName"):
+                fileName = arg
+            elif opt in ("-i", "--iLabel"):
+                I = arg
+            elif opt in ("-o", "--oLabel"):
+                O = arg
+    fileName = fileName or "GG"
+    I = I or "a b"
+    O = O or "o"
+if __name__ == "__main__":
+   main(sys.argv[1:])
+print("file Name is",fileName)
+print ("Input labels are ", I)
+print ("Output labels are ", O)
+
+
+f = open(fileName+".cmd","w")
 # print("Input your fileName:\n")
 # file name = input()
 # print("Input your stepSize:")
 # stepSize = input()
-fileName = str(input("Input your file name [default is 'GG']:\n") or "GG")
-f = open(fileName+".cmd","w")
-stepSize = int(input("Enter your StepSize [default is '50':]\n") or 50)
-labels = [str("")]*20
-I = str(input("Input your input labels [you can add up to 20 labels] [ default is 'a b']:\n") or "a b")
-O = str(input("Input your output labels [ default is 'o']:\n") or "o")
+
 numOfI = 0
 for i in I :
     if i != ' ' :
@@ -45,6 +76,8 @@ for i in range(numOfIter):
 f.write(txt)
 f.close()
 os.system("open "+f.name)
+
+
 
 ### Made by abdo25 ###
 ### github.com/abdo20050 ###
