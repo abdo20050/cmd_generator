3de2b26c63e2e3111752939ff2564ddcb39b54ee
abdo25 <aabdo20050@gmail.com>
2022-09-24 16:37:56 +0300

    cmd_gen V0.1

--- cmd_gen.py
+++ cmd_gen.py
@@ -1,3 +1,7 @@
+# Bismellah 
+#This code is made only for educational purposes, any misuse programmer won't be responsible
+#Description: This code used to generate cmd files for "irsim" simulator
+import os
 def grayGen(num):
     return num ^ (num >> 1)
 
@@ -5,10 +9,11 @@ def grayGen(num):
 # file name = input()
 # print("Input your stepSize:")
 # stepSize = input()
-print("Input your labels [you can add up to 20 labels]:\n")
+fileName = str(input("Input your file name [default is 'GG']:\n") or "GG")
+f = open(fileName+".cmd","w")
+stepSize = int(input("Enter your StepSize [default is '50':]\n") or 50)
 labels = ['']*20
-I = "a b c d"
-I = input()
+I = str(input("Input your labels [you can add up to 20 labels] [ default is 'a b']:\n") or "a b")
 numOfI = 0
 for i in I :
     if i != ' ' :
@@ -18,22 +23,20 @@ numOfIter = 2**numOfI
 signals = [[False]*numOfI]*(numOfIter)
 for i in range(numOfIter):
     signals[i] =  grayGen(i)
+txt = ""
+txt += "stepsize "+str(stepSize)+'\n'
+txt += "ana vdd gnd "+I + '\n'
+txt += "w vdd gnd "+I + '\n'
+txt += "h vdd \n"
+txt += "l gnd \n"
 
-print("stepsize 50")
-print("ana vdd gnd "+I)
-print("w vdd gnd "+I)
-print("h vdd")
-print("l gnd")
 for i in range(numOfIter):
     for j in range(numOfI):
-        print(('h' if (signals[i]>>j & 1) else 'l')+' '+labels[j])
-    print('s')
-# print(numOfI)
-# for i in labels:
-#     if i == '':
-#         break
-#     print(i)
-#     print(' ')
-#print(I)
+        txt += ('h' if (signals[i]>>j & 1) else 'l')+' '+labels[j] + '\n'
+    txt += "s\n"
+f.write(txt)
+f.close()
+os.system("open "+f.name)
 
-
+### Made by abdo25 ###
+### github.com/abdo20050 ###
